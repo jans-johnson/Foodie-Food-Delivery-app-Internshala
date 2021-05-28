e3170f3fbb6fba2ae4b60a9eccbe3160322801eb
Jans Johnson <jaansjohnson@gmail.com>
2021-05-28 15:57:59 +0530

    added restaurantMenuActivity and components

--- app/src/main/java/com/jns/foodie/activity/RestaurantMenuActivity.kt
+++ app/src/main/java/com/jns/foodie/activity/RestaurantMenuActivity.kt
@@ -1,13 +1,23 @@
 package com.jns.foodie.activity
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.Settings
 import android.view.MenuItem
-import android.widget.ImageView
-import android.widget.TextView
+import android.view.View
+import android.widget.*
 import androidx.appcompat.widget.Toolbar
+import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.android.volley.Response
+import com.android.volley.toolbox.JsonObjectRequest
+import com.android.volley.toolbox.Volley
 import com.jns.foodie.R
+import com.jns.foodie.adapter.MenuAdapter
+import com.jns.foodie.model.RestaurantMenu
+import com.jns.foodie.utils.ConnectionManager
+import org.json.JSONException
 
 class RestaurantMenuActivity : AppCompatActivity() {
 
@@ -16,9 +26,13 @@ class RestaurantMenuActivity : AppCompatActivity() {
     lateinit var layoutManager: RecyclerView.LayoutManager
     lateinit var ivFavIcon:ImageView
     lateinit var tvRating:TextView
+    lateinit var btnProceedToCart: Button
+    lateinit var menuProgressLayout:RelativeLayout
+    lateinit var menuAdapter: MenuAdapter
 
     lateinit var restaurantId: String
     lateinit var restaurantName: String
+    var restaurantMenuList = arrayListOf<RestaurantMenu>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -27,6 +41,10 @@ class RestaurantMenuActivity : AppCompatActivity() {
         ivFavIcon=findViewById(R.id.ivFavIcon)
         tvRating=findViewById(R.id.tvRating)
         toolbarMenu=findViewById(R.id.toolbarMenu)
+        btnProceedToCart=findViewById(R.id.btnProceedToCart)
+        menuProgressLayout=findViewById(R.id.menuProgressLayout)
+        recyclerViewMenu=findViewById(R.id.RecyclerViewMenu)
+        layoutManager = LinearLayoutManager(this)
 
         restaurantId = intent.getStringExtra("restaurantId")!!
         restaurantName = intent.getStringExtra("restaurantName")!!
@@ -42,13 +60,43 @@ class RestaurantMenuActivity : AppCompatActivity() {
 
     }
 
+    //Once items added to cart, If user press back , items will be cleared
+    override fun onBackPressed() {
+        if (menuAdapter.getSelectedItemCount() > 0) {
+
+            val alterDialog = androidx.appcompat.app.AlertDialog.Builder(this)
+            alterDialog.setTitle("Alert!")
+            alterDialog.setMessage("Going back will remove everything from cart")
+            alterDialog.setPositiveButton("Okay") { _, _ ->
+                super.onBackPressed()
+            }
+            alterDialog.setNegativeButton("Cancel") { _, _ ->
+
+            }
+            alterDialog.show()
+        } else {
+            super.onBackPressed()
+        }
+    }
+
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
 
         when (item.itemId) {
             android.R.id.home -> {
+                if (menuAdapter.getSelectedItemCount() > 0) {
+                    val alterDialog = androidx.appcompat.app.AlertDialog.Builder(this)
+                    alterDialog.setTitle("Alert!")
+                    alterDialog.setMessage("Going back will remove everything from cart")
+                    alterDialog.setPositiveButton("Okay") { _, _ ->
+                        super.onBackPressed()
+                    }
+                    alterDialog.setNegativeButton("Cancel") { _, _ ->
 
-                super.onBackPressed()
-
+                    }
+                    alterDialog.show()
+                } else {
+                    super.onBackPressed()
+                }
             }
         }
         return super.onOptionsItemSelected(item)
@@ -59,5 +107,95 @@ class RestaurantMenuActivity : AppCompatActivity() {
         supportActionBar?.title = restaurantName
         supportActionBar?.setHomeButtonEnabled(true)
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
+    }
+
+    override fun onResume() {
+
+        if (ConnectionManager().checkConnectivity(this)) {
+            if (restaurantMenuList.isEmpty()){
+                menuProgressLayout.visibility= View.VISIBLE
+
+                try {
+                    val queue=Volley.newRequestQueue(this)
+                    val url="http://13.235.250.119/v2/restaurants/fetch_result/$restaurantId"
+
+                    val jsonObjectRequest = object : JsonObjectRequest(
+                            Method.GET,
+                            url,
+                            null,
+                            Response.Listener {
+                                val response = it.getJSONObject("data")
+                                val success = response.getBoolean("success")
+
+                                if (success) {
+                                    restaurantMenuList.clear()
+                                    val data = response.getJSONArray("data")
+
+                                    for (i in 0 until data.length()) {
+                                        val restaurant = data.getJSONObject(i)
+                                        val menuObject = RestaurantMenu(
+                                                restaurant.getString("id"),
+                                                restaurant.getString("name"),
+                                                restaurant.getString("cost_for_one")
+                                        )
+
+                                        restaurantMenuList.add(menuObject)
+                                        menuAdapter = MenuAdapter(
+                                                this,
+                                                restaurantId,
+                                                restaurantName,
+                                                btnProceedToCart,
+                                                restaurantMenuList
+                                        )
+
+                                        recyclerViewMenu.adapter = menuAdapter
+                                        recyclerViewMenu.layoutManager = layoutManager
+                                    }
+                                }
+                                menuProgressLayout.visibility = View.INVISIBLE
+                            },
+                            Response.ErrorListener {
+                                println("Error12menu is $it")
+
+                                Toast.makeText(
+                                        this,
+                                        "Some Error occurred!!!",
+                                        Toast.LENGTH_SHORT
+                                ).show()
+                                menuProgressLayout.visibility = View.INVISIBLE
+                            }) {
+                        override fun getHeaders(): MutableMap<String, String> {
+                            val headers = HashMap<String, String>()
+                            headers["Content-type"] = "application/json"
+                            headers["token"] = "c1b1256d960512"
+                            return headers
+                        }
+                    }
+                    queue.add(jsonObjectRequest)
+                }catch (e: JSONException) {
+                    Toast.makeText(
+                            this,
+                            "Some Unexpected error occurred!!!",
+                            Toast.LENGTH_SHORT
+                    ).show()
+                }
+            }
+        } else {
+
+            val alterDialog = androidx.appcompat.app.AlertDialog.Builder(this)
+            alterDialog.setTitle("No Internet")
+            alterDialog.setMessage("Check Internet Connection!")
+            alterDialog.setPositiveButton("Open Settings") { _, _ ->
+                val settingsIntent = Intent(Settings.ACTION_SETTINGS)
+                startActivity(settingsIntent)
+            }
+            alterDialog.setNegativeButton("Exit") { _, _ ->
+                finishAffinity()
+            }
+            alterDialog.setCancelable(false)
+            alterDialog.create()
+            alterDialog.show()
+        }
+        super.onResume()
     }
 }
---
+++ app/src/main/java/com/jns/foodie/adapter/MenuAdapter.kt
@@ -0,0 +1,91 @@
+package com.jns.foodie.adapter
+
+import android.content.Context
+import android.content.Intent
+import android.graphics.Color
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import android.widget.RelativeLayout
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.jns.foodie.R
+import com.jns.foodie.model.RestaurantMenu
+
+class MenuAdapter (val context: Context,
+                   val restaurantId: String,
+                   val restaurantName: String,
+                   val buttonProceedToCart: Button,
+                   val restaurantMenu: ArrayList<RestaurantMenu>) : RecyclerView.Adapter<MenuAdapter.ViewHolderMenu>(){
+
+    var itemSelectedCount: Int = 0
+    lateinit var proceedToCart: Button
+    var itemsSelectedId = arrayListOf<String>()
+
+    class ViewHolderMenu(view: View): RecyclerView.ViewHolder(view)
+    {
+        val tvSerialNumber: TextView = view.findViewById(R.id.tvSerialNumber)
+        val tvItemName: TextView = view.findViewById(R.id.tvItemName)
+        val tvItemPrice: TextView = view.findViewById(R.id.tvItemPrice)
+        val btnAddToCart: Button = view.findViewById(R.id.btnAddToCart)
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolderMenu {
+        val view = LayoutInflater.from(parent.context)
+            .inflate(R.layout.restaurant_menu_single_row, parent, false)
+
+        return ViewHolderMenu(view)
+    }
+
+    override fun getItemCount(): Int {
+        return restaurantMenu.size
+    }
+
+    override fun onBindViewHolder(holder: ViewHolderMenu, position: Int) {
+        val restaurantMenuItem = restaurantMenu[position]
+        proceedToCart = buttonProceedToCart
+
+        /*buttonProceedToCart.setOnClickListener {
+            val intent = Intent(context, CartActivity::class.java)
+            intent.putExtra("restaurantId", restaurantId)
+            intent.putExtra("restaurantName", restaurantName)
+            intent.putExtra("selectedItemsId", itemsSelectedId)
+            context.startActivity(intent)
+        }*/
+
+        holder.btnAddToCart.setOnClickListener {
+
+            if (holder.btnAddToCart.text.toString() == "Remove") {
+
+                itemSelectedCount--
+                itemsSelectedId.remove(holder.btnAddToCart.tag.toString())
+                holder.btnAddToCart.text = "Add"
+                holder.btnAddToCart.setBackgroundColor(Color.rgb(31, 171, 137))
+
+            } else {
+
+                itemSelectedCount++
+                itemsSelectedId.add(holder.btnAddToCart.tag.toString())
+                holder.btnAddToCart.text = "Remove"
+                holder.btnAddToCart.setBackgroundColor( Color.rgb(255, 196, 0))
+            }
+
+            if (itemSelectedCount > 0) {
+                proceedToCart.visibility = View.VISIBLE
+            } else {
+                proceedToCart.visibility = View.GONE
+            }
+        }
+
+        holder.btnAddToCart.tag = restaurantMenuItem.id + ""
+        holder.tvSerialNumber.text = (position + 1).toString()
+        holder.tvItemName.text = restaurantMenuItem.name
+        holder.tvItemPrice.text = "Rs. ${restaurantMenuItem.cost_for_one}"
+    }
+
+    fun getSelectedItemCount(): Int {
+        return itemSelectedCount
+    }
+
+}
